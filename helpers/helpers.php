68f4d8258e639116e7ab44e95b1d5923b7dfb3a3
lhp9-tg <thomas.gouel@gmail.com>
2023-01-25 10:48:42 +0100

    Home réparée

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -239,23 +239,23 @@ function displayhome($param)
 
     foreach ($homeflux as $item) {
         if ($i <= $maxarticle) {
-            $i++;
-
+            
             $originalDate = $item->pubDate;
             $newDate = date('d/m/Y H:i', strtotime($originalDate));
             
-            array_push($testdate, $newDate);
-
             if (in_array($newDate, $testdate)) {
                 continue;
             }
+
+            array_push($testdate, $newDate);
+            $i++;
 
             echo '
             <div class="container mx-auto">
             <div class="row actus my-2 mx-0">
 
                 <div class="actus-img col-4"><img src="../assets/img/news-paper.png" alt=""></div>
-                <div class="actus-article col-8">
+                <div class="actus-article col-lg-8">
                 <p class="text-center"> <a class="title" href="' . $item->link . '" target="_blank">' . $item->title . '</a></p>
                     <p class="date text-center">' . $newDate . '</p>
                 </div>
--- views/home.php
+++ views/home.php
@@ -50,16 +50,10 @@ else {
 }
 
     ?>
- 
-        <footer>
-          
-        
-         <p class="mb-0 text-center">© 2023 </p>
-               
-            
-        </footer>
+    <footer>
+        <p class="mb-0 text-center">© 2023 </p>       
+    </footer>
 
-  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script src="script.js"></script>
 </body>
