58b59c739a14a7b8a3e66466c8527ab49bed7fdc
David Godard <davidgodard76700@gmail.com>
2023-01-25 10:40:52 +0100

    maj

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -108,7 +108,7 @@ function rss_reader($rss_feed) // Fonction qui prend en paramètre l'URL du flux
                 <div class="row sport my-2 mx-0">
 
                     <div class="actus-img col-12 col-lg-4"><img src="'.$photo_url.'" alt=""></div>
-                    <div class="actus-article col-12 col-8 mt-3">
+                    <div class="actus-article col-12 col-lg-8 mt-3">
                     <p class="text-center"> <a class="title" href="' . $item->link . '" target="_blank">' . $item->title . '</a></p>
                         <p class="date text-center">' . $newDate . '</p>
                     </div>
